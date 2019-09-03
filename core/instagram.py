ba2ee2057d1e131aa8374c284cbae3a9564ccc16
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-09-02 23:52:13 -0300

    criando atributos e metodos

---
+++ core/instagram.py
@@ -0,0 +1,34 @@
+class User:
+	def __init__(self):
+		self._username = None
+		self._password = None
+		self._user = None
+
+	"-----------------------------get-and-set-------------------"
+	@property
+	def username(self):
+		return self._username
+	
+	@username.setter
+	def username(self, valor):
+		self._username = valor
+	
+	@property
+	def password(self):
+		return self._password
+	
+	@password.setter
+	def password(self, valor):
+		self._password = valor
+		
+	@property
+	def user(self):
+		return self._user
+	
+	@user.setter
+	def user(self, valor):
+		self._user = valor
+
+	"-----------------------------method---------------------"
+	def get_last_posts(self):
+		pass
