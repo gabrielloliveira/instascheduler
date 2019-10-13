e1951d08771c32f497f988f6fca90d707134d6c8
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-13 09:02:53 -0300

    resolvido conflitos da conexão com o banco fechada

--- server/connector.py
+++ server/connector.py
@@ -17,15 +17,30 @@ class Connector():
         return Connector.__instance
             
     def __init__(self):
-        self._conn = Connector.connection
-        self._cursor = self._conn.cursor()  
+        self._conn = None
+        self._cursor = None
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self._db_path = os.path.join(BASE_DIR, "db.sqlite3")
+
+    def connect_db(self):
+        self._conn = sqlite3.connect(self._db_path)
+        self._cursor = self._conn.cursor()
 
     def search_user(self, email, password):
-        result = self._cursor.execute("""
-        SELECT * FROM user WHERE email=? and password=?
-        """, (email, password))
+        self.connect_db()
 
-        return result.fetchone()
+        try:
+            with self._conn:
+                result = self._cursor.execute("""
+                SELECT * FROM user WHERE email=? and password=?
+                """, (email, password))
+
+                return result.fetchone()
+
+            return None
+
+        finally:
+            self._conn.close()
 
     def close(self):
         self._conn.close()
