90366faa17b334b1a00f374db468b147916f02ed
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-15 15:13:11 -0300

    alterado função para criar uma postagem

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from connector import Connector
+from datetime import datetime
 import socket, threading
 import socket
 import pickle
@@ -30,20 +31,27 @@ def add_insta(received):
     return result
 
 
-def post(msg):
-    nome = msg['nome']
-    dados = msg['dados']
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    img_path = os.path.join(BASE_DIR, f"recebidos/{nome}")
+def schedule_posting(received):
+    user = received['user']
+    img = received['img']
+    subtitle = received['subtitle']
+    location = received['location']
+    instagram = received['instagram']
+    date = received['date']
 
-    print("======= caminoho", img_path)
-    
+    date_image = f'{datetime.now().year}-' + f'{datetime.now().month}-' + f'{datetime.now().day}'
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    img_path = os.path.join(BASE_DIR, f"uploads/{date_image}-{user}-{nome}")
+
+    # Salvando a imagem na pasta upload com o nome padrão : yyyy-mm-dd-email-name.jpeg
     with open((img_path), 'wb') as f:
         f.write(dados)
         f.close()
 
-    print("======= terminou o processamento")
-
+    conn = Connector()
+    result = conn.add_schedule(img_path, subtitle, location, instagram, date, user)
+    return result
     
 class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket):
